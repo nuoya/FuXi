e7878f0ff7814e69167addc9a3d8b98a1513a333
cliff xuan <cliff.xuan@gmail.com>
2015-07-16 14:02:32 +0000

    fixed path for shell script

--- examples/test1.example.sh
+++ examples/test1.example.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FuXi \
 --closure \
 --output="n3" \
@@ -5,8 +7,8 @@ FuXi \
 --dlp \
 --input-format="n3" \
 --method=bfp \
---rules=./test.formulae.n3 \
+--rules=$DIR/test1.formulae.n3 \
 --why="SELECT ?x ?y WHERE { ?x test:has_child ?y  }" \
 --why="SELECT ?x ?y WHERE { ?x test:has_sibling ?y  }" \
-./test.ontology.n3 \
-./test.individuals.n3 
+$DIR/test1.ontology.n3 \
+$DIR/test1.individuals.n3
